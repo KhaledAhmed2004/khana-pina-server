32b82955c12bf7f215e433e5b4cbb89d629a824e
KhaledAhmed2004 <khaledahmednayeem2004.com>
2023-11-09 00:36:23 +0600

    make purchaseOrder route

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
+app.use(express.json());
 app.use(cors());
 
 // MongoDB Configuration
@@ -23,21 +24,49 @@ const client = new MongoClient(uri, {
 // MongoDB Connection
 async function run() {
   try {
-    await client.connect();
-
+    // Define the testimonialCollection
     const testimonialCollection = client
       .db("restaurant")
       .collection("testimonial");
 
+    //   define the purchaseOrders
+    const purchaseOrdersCollection = client
+      .db("restaurant")
+      .collection("purchaseOrders");
+
+    // Define the foodItemCollection
+    const foodItemsCollection = client.db("restaurant").collection("foodItems");
+
     app.get("/testimonial", async (req, res) => {
-      const result = await testimonialCollection.find().toArray();
-      res.send(result);
+      try {
+        const result = await testimonialCollection.find().toArray();
+        res.json(result);
+      } catch (error) {
+        console.log("Error fetching testimonials:", error);
+      }
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    app.post("/purchaseOrders", async (req, res) => {
+      try {
+        const order = req.body;
+        console.log(order);
+        const result = await purchaseOrdersCollection.insertOne(order);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    app.post("/foodItems", async (req, res) => {
+      try {
+        const body = req.body;
+        console.log(body);
+        const result = await foodItemsCollection.insertOne(body);
+        res.send(result);
+      } catch (error) {
+        console.log("Error processing the JSON data:", error);
+      }
+    });
   } catch (error) {
     console.log("MongoDB connection error:", error);
   }
